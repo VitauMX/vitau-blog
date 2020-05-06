bcf99a632feb978b3d809436b2df63a5031d4a0a
aka-cronos <jorge.escamilla89@gmail.com>
2020-05-06 10:31:45 -0500

    Change to use playground for queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,9 @@
 const postcssPresetEnv = require("postcss-preset-env")
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
