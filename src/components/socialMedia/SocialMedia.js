657dd3496ccf3d4389b05a5e37a19655e898e076
aka-cronos <jorge.escamilla89@gmail.com>
2020-05-07 11:27:49 -0500

    Add social Media

---
+++ src/components/socialMedia/SocialMedia.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import classNames from 'classnames'
+import './socialMedia.scss'
+
+const SocialMedia = (props) => {
+  const { isWhite } = props
+
+  let styles = classNames('socialMedia', {
+    'socialMedia--isWhite': isWhite === true,
+  })
+
+  return (
+    <div className={styles}>
+      <a
+        className="socialMedia-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://facebook.com/vitau.mx"
+      >
+        <i className="fab fa-facebook-square" />
+      </a>
+
+      <a
+        className="socialMedia-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://instagram.com/vitau.mx"
+      >
+        <i className="fab fa-instagram"></i>
+      </a>
+    </div>
+  )
+}
+
+export default SocialMedia
