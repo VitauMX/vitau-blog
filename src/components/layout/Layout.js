e04953eeeb3651c546b402064a18126b35e3b24c
aka-cronos <jorge.escamilla89@gmail.com>
2020-05-07 10:24:10 -0500

    Add Layout

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+
+// Styles
+import "normalize.css"
+import { defaultStyles } from "vitau-ui-framework"
+import "../../styles/styles.scss"
+import "./layout.scss"
+
+import MainHeader from "../mainHeader/MainHeader"
+// import Footer from "../footer/Footer"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Helmet>
+        <html lang="es" />
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
+          rel="stylesheet"
+        />
+      </Helmet>
+
+      <MainHeader />
+
+      <main className="main">{children}</main>
+
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default Layout
